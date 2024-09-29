f5284ecdb9209de3859db67363c84c80de213460
Hector Macassa <cbmacassa@gmail.com>
2024-09-29 21:38:19 +0200

    HashMap loop

---
+++ src/HashMapLoop.java
@@ -0,0 +1,14 @@
+import java.util.HashMap;
+public class HashMapLoop {
+    public static void main(String[] args) {
+        HashMap<String, String> capitalCities = new HashMap<String, String>();
+        capitalCities.put("USA", "Washington");
+        capitalCities.put("UK", "London");
+        capitalCities.put("Germany", "Berlin");
+        capitalCities.put("Kenya", "Nairobi");
+
+        for (String i: capitalCities.keySet()) {
+            System.out.println(i);
+        }
+    }
+}
