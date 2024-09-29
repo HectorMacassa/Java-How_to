08ae35b72652d8d7488f2dd6cfbdc75e8b3014d7
Hector Macassa <cbmacassa@gmail.com>
2024-09-29 18:08:50 +0200

    Smallest element in array

---
+++ src/SmallestElement.java
@@ -0,0 +1,14 @@
+public class SmallestElement {
+    public static void main(String[] args) {
+        int[] ages = {40, 22, 19, 29, 35, 23, 77, 60};
+
+        int lowestAge = ages[0];
+
+        for (int age: ages) {
+            if (lowestAge > age) {
+                lowestAge = age;
+            }
+        }
+        System.out.println(lowestAge);
+    }
+}
